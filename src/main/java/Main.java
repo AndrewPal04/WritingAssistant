87558abc2530ad2437a95300ac1b1019f1e6ec4b
AndrewPal04 <aapalacios@cpp.edu>
2025-11-04 12:49:38 -0800

    Cleaned up file structure and added pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+package ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        // will wire up controller + view later
     }
 }
---
+++ src/main/java/model/APIClient.java
@@ -0,0 +1,5 @@
+package model;
+
+public class APIClient {
+    // Will implement later
+}
